8218e4ef91184d9197db7b5288845d19512e4537
Xmow <47485034+xmow49@users.noreply.github.com>
2020-06-12 22:42:40 +0200

    Update variable.h

--- variable.h
+++ variable.h
@@ -1,3 +1,7 @@
 volatile long int postionEncodeur = 0;
 int motor_value = 255;
 unsigned int integerValue = 0; // Max value is 65535
+
+bool obstacle_Endstop = 0;
+bool endstop_G = 1;
+bool endstop_D = 1;
